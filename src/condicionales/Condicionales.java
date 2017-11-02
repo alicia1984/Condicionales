488b6f6f0296fa964262365069c80e7e3958fef6
alicia1984 <alicia28031@hotmail.com>
2017-11-02 19:23:16 -0300

    operador  de  comparacion == en  el  if

--- src/condicionales/Condicionales.java
+++ src/condicionales/Condicionales.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package condicionales;
-
+import javax.swing.JOptionPane;
 /**
  *
  * @author LP 13
@@ -14,7 +14,25 @@ public class Condicionales {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
+       float numero1, numero2;
+       numero1 = Float.parseFloat(JOptionPane.showInputDialog("Ingrese el Primer  Numero"));
+       numero2 = Float.parseFloat(JOptionPane.showInputDialog("Ingrese el Segundo  Numero"));
+       
+        if (numero1 == numero2)//aqui va una  condicion
+        {
+            
+        //si la  condicion se cumple  se ejecuta este  codigo    
+        JOptionPane.showMessageDialog(null, "Son Iguales");
+        
+        }
+        
+        else 
+        {
+        //si  no  se cumple la  condicion s e ejecuta esto
+            
+        }
         // TODO code application logic here
     }
     
